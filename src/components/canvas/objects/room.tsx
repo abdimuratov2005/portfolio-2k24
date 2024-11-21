2e5df020b0604a8ba8f5252e5dd4a263d9c35c59
Abdulla <abdimuratov2005@yandex.com>
2024-11-21 18:02:51 +0500

    first-commit

--- src/components/canvas/objects/room.tsx
+++ src/components/canvas/objects/room.tsx
@@ -1,18 +1,14 @@
 import { useGLTF } from '@react-three/drei'
-
+// @ts-nocheck
 export default function Room() {
     const { nodes, materials } = useGLTF('/blender_models/portfolio.glb')
 
     return (
         <group dispose={null}>
             <mesh
-                // @ts-ignore
                 castShadow
-                // @ts-ignore
                 receiveShadow
-                // @ts-ignore
                 geometry={nodes.Plane.geometry}
-                // @ts-ignore
                 material={nodes.Plane.material}
                 position={[-2, -1, 0]}
                 scale={[0.7, 0.7, 0.7]}
