93a9b8555a8cffe8189bae1b93ecb09e857d7426
Abdulla <abdimuratov2005@yandex.com>
2024-11-21 18:10:32 +0500

    first-commit

--- src/components/canvas/objects/room.tsx
+++ src/components/canvas/objects/room.tsx
@@ -1,21 +1,15 @@
-import { useGLTF } from '@react-three/drei'
-// @ts-nocheck
-export default function Room() {
-    const { nodes, materials } = useGLTF('/blender_models/portfolio.glb')
+import {useLoader} from "@react-three/fiber";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
 
+export default function Room() {
+    const gltf = useLoader(GLTFLoader, '/blender_models/portfolio.glb')
     return (
-        <group dispose={null}>
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Plane.geometry}
-                material={nodes.Plane.material}
-                position={[-2, -1, 0]}
-                scale={[0.7, 0.7, 0.7]}
-            />
-        </group>
+        <primitive
+            object={gltf.scene}
+            castShadow
+            receiveShadow
+            position={[-2, -1, 0]}
+            scale={[0.7, 0.7, 0.7]}
+        />
     )
 }
-
-useGLTF.preload('/blender_models/portfolio.glb')
-
