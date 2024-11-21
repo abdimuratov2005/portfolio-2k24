6503447c7e125f850ad2b6d0414f3a246e081e5d
Abdulla <abdimuratov2005@yandex.com>
2024-11-21 17:59:07 +0500

    first-commit

--- src/components/canvas/objects/room.tsx
+++ src/components/canvas/objects/room.tsx
@@ -6,9 +6,13 @@ export default function Room() {
     return (
         <group dispose={null}>
             <mesh
+                // @ts-ignore
                 castShadow
+                // @ts-ignore
                 receiveShadow
+                // @ts-ignore
                 geometry={nodes.Plane.geometry}
+                // @ts-ignore
                 material={nodes.Plane.material}
                 position={[-2, -1, 0]}
                 scale={[0.7, 0.7, 0.7]}
